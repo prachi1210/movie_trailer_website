6da46e50fa51802aa64a5f7e3626a2f3bc0fd4ff
Prachi <prachi121096@gmail.com>
2016-06-11 17:51:06 +0530

    first commit

---
+++ movie.py
@@ -0,0 +1,8 @@
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image,trailer_url):
+        self.title=movie_title
+        self.storyline=movie_storyline
+        self.poster=poster_image
+        self.trailer=trailer_url
+        
+    
---
+++ movie_list.py
@@ -0,0 +1,5 @@
+import movie
+
+toy_story=movie.Movie("Toy story", "sample story", "http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest/scale-to-width-down/320?cb=20140816182710", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
