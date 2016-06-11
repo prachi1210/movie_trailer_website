c658d0292e1112c54e400849d1976fb8edf0c18a
Prachi <prachi121096@gmail.com>
2016-06-11 21:03:37 +0530

    created instances successfully

--- movie.py
+++ movie.py
@@ -1,9 +1,10 @@
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_title, poster_image):
+    def __init__(self, movie_title, poster_image,trailer):
         self.title=movie_title
         self.poster=poster_image
+        self.trailer_url=trailer
 
     def show_trailer(self):
         webbrowser.open(self.trailer)
--- movie_list.py
+++ movie_list.py
@@ -3,23 +3,43 @@ from urllib2 import Request, urlopen, URLError
 import json
 import movie
 import fav_movies
+from bs4 import BeautifulSoup
+
+def get_youtube_url(title):
+    query = urllib.quote_plus(title+"trailer")
+    url = "https://www.youtube.com/results?search_query=" + query
+    response = urllib.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html,"html.parser")
+
+    for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+        youtube_url= 'https://www.youtube.com' + vid['href']
+        break
+    return youtube_url
+
+def get_poster(title):
+    title =raw_input('Type in your favourite movie ')
+    omdb_request = Request('http://www.omdbapi.com/?s='+urllib.quote_plus(title))
+    response = urlopen(omdb_request)
+    data = response.read()
+    d=json.loads(data)
+    if 'false' in d:
+        poster= "No such movie"
+    else:
+        poster=d['Search'][0]['Poster']
+    return poster
+    
 
 movies_list=[]
 i=0
 while(i<3):
     title =raw_input('Type in your favourite movie ')
-    request = Request('http://www.omdbapi.com/?s='+urllib.quote_plus(title))
-    try:
-        response = urlopen(request)
-        data = response.read()
-        d=json.loads(data)
-        poster=d['Search'][0]['Poster']
-        movies_list.append(movie.Movie(title, poster))
-    except URLError:
-        print("Error")
+    movies_list.append(movie.Movie(title, get_poster(title), get_youtube_url(title)))
     i=i+1
+    
 for fav_movie in movies_list:
     print(fav_movie.poster)
+    print(fav_movie.trailer_url)                   
 
 
 
